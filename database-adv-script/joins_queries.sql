32ec4a64ccf81282cbc9e56f6941b8374cd92ea7
Joseph Ahor Abandoh-Sam <abandoh@gmail.com>
2025-07-04 18:40:04 +0000

    updated joins_queries with changes

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,30 @@
+-- a query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+SELECT 
+  b.booking_id,
+  b.start_date,
+  b.end_date,
+  u.user_id,
+  u.email,
+  CONCAT(u.first_name, ' ', u.last_name) AS guest_name
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.user_id;
+
+-- a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
+  p.property_id,
+  p.name AS property_name,
+  r.review_id,
+  r.rating,
+  r.comment
+FROM properties p
+LEFT JOIN reviews r ON p.property_id = r.property_id;
+
+-- a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+
+SELECT 
+  u.user_id,
+  u.email,
+  b.booking_id,
+  b.start_date
+FROM users u
+FULL OUTER JOIN bookings b ON u.user_id = b.user_id;
